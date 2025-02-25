3833c4da22e284d8472c456791700ba38d0edbea
walaa-zahran40 <walaa.zahran@corplease.com.eg>
2025-02-25 19:15:10 +0200

    workflow actions button styling

--- src/app/pages/crm/clients/companies/add-address/add-address.component.ts
+++ src/app/pages/crm/clients/companies/add-address/add-address.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { MessageService } from 'primeng/api';
+import { UploadEvent } from 'primeng/fileupload';
 
 @Component({
   selector: 'app-add-address',
   standalone: false,
   templateUrl: './add-address.component.html',
-  styleUrl: './add-address.component.scss'
+  styleUrl: './add-address.component.scss',
 })
 export class AddAddressComponent {
+  constructor(private messageService: MessageService) {}
 
+  addDocument() {
+    console.log('added');
+  }
+  onBasicUploadAuto(event: UploadEvent) {
+    this.messageService.add({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded with Auto Mode',
+    });
+  }
 }
