026f7e7e999100119c52cc7da6565ea0b440c952
walaa-zahran40 <walaa.zahran@corplease.com.eg>
2025-03-10 20:41:20 +0200

    client journey without double checking responsiveness, padding and spacing

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -8,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class NavBarComponent {
   checked = true;
+  darkMode: boolean = false;
+
+  toggleDarkMode() {
+    this.darkMode = !this.darkMode;
+    if (this.darkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
 }
