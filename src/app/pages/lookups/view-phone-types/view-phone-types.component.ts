e4eb5c9f4ba5711b3c4ffe51eb471b92173a0e8b
walaa-zahran40 <walaa.zahran@corplease.com.eg>
2025-03-15 17:03:35 +0200

    view phone types

---
+++ src/app/pages/lookups/view-phone-types/view-phone-types.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-phone-types',
+  standalone: false,
+  templateUrl: './view-phone-types.component.html',
+  styleUrl: './view-phone-types.component.scss'
+})
+export class ViewPhoneTypesComponent {
+
+}
