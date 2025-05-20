9a6ec3f13de542c857c000810017eb59c5777380
walaa-zahran40 <walaa.zahran@corplease.com.eg>
2025-05-20 13:24:16 +0300

    ++

--- src/app/pages/crm/clients/components/clients/company-individual/add-client/add-client.component.ts
+++ src/app/pages/crm/clients/components/clients/company-individual/add-client/add-client.component.ts
@@ -16,7 +16,6 @@ import { Sector } from '../../../../../../lookups/store/sectors/sector.model';
 import { ClientsFacade } from '../../../../store/_clients/allclients/clients.facade';
 import { Individual } from '../../../../store/_clients/individuals/individual.model';
 import { ClientIdentityTypesFacade } from '../../../../store/client-identity-types/client-identity-types.facade';
-import { loadSectorById } from '../../../../store/sector-drop-down/sector.actions';
 import { selectAllSubSectors } from '../../../../store/sub-sector-drop-down/sub-sector.selectors';
 import { IndividualsFacade } from '../../../../store/_clients/individuals/individuals.facade';
 import { Client } from '../../../../store/_clients/allclients/client.model';
