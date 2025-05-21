1807ca66495a695c35099203dde945df5c98c42c
walaa-zahran40 <walaa.zahran@corplease.com.eg>
2025-05-21 20:53:05 +0300

    search wizard done

--- src/app/pages/crm/clients/components/client-activities/client-activity-wizard/client-activity-wizard.component.ts
+++ src/app/pages/crm/clients/components/client-activities/client-activity-wizard/client-activity-wizard.component.ts
@@ -9,123 +9,130 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ClientActivityWizardComponent {
   cards: any[] = [];
-  clientId!: number;
-
+  originalCards: any[] = [];
   constructor(private router: Router, private route: ActivatedRoute) {}
   ngOnInit(): void {
-    this.clientId = +this.route.snapshot.paramMap.get('clientId')!;
-    console.log('Loaded clientId:', this.clientId);
-    this.cards = [
-      [
-        {
-          imgUrl: '/assets/images/shared/card/upload.svg',
-          imgAlt: 'upload',
-          title: 'Upload Documents',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: `/crm/clients/view-upload-documents/${this.clientId}`,
-        },
-        {
-          imgUrl: '/assets/images/shared/card/add.svg',
-          imgAlt: 'add',
-          title: 'Add Address',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: `crm/clients/view-client-addresses/${this.clientId}`,
-        },
-        {
-          imgUrl: '/assets/images/shared/card/sales.svg',
-          imgAlt: 'sales',
-          title: 'Sales Turnover',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: `/crm/clients/view-sales-turnover/${this.clientId}`,
-        },
-      ],
-      [
-        {
-          imgUrl: '/assets/images/shared/card/phone.svg',
-          imgAlt: 'phone',
-          title: 'Phone Number',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: `/crm/clients/view-phone-number/${this.clientId}`,
-        },
-        {
-          imgUrl: '/assets/images/shared/card/contact.svg',
-          imgAlt: 'contact',
-          title: 'Contact Person',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-           link: `/crm/clients/view-contact-person/${this.clientId}`,
-        },
-        {
-          imgUrl: '/assets/images/shared/card/cr.svg',
-          imgAlt: 'cr',
-          title: 'CR Authority Office',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: `/crm/clients/view-client-cr-authority-offices/${this.clientId}`,
-        },
-      ],
-      [
-        {
-          imgUrl: '/assets/images/shared/card/tax.svg',
-          imgAlt: 'tax',
-          title: 'TAX Authority Office',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: `/crm/clients/view-client-tax-authority-offices/${this.clientId}`,
-        },
-        {
-          imgUrl: '/assets/images/shared/card/central.svg',
-          imgAlt: 'central',
-          title: 'Central Bank Info',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: `/crm/clients/view-client-central-bank-info/${this.clientId}`,
-        },
-        {
-          imgUrl: '/assets/images/shared/card/share.svg',
-          imgAlt: 'share',
-          title: 'Share Holders',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: `/crm/clients/view-client-share-holders/${this.clientId}`,
-        },
-      ],
-      [
-        {
-          imgUrl: '/assets/images/shared/card/tml.svg',
-          imgAlt: 'tml',
-          title: 'TML Officer',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: `/crm/clients/view-client-tml-officers/${this.clientId}`,
-        },
-        {
-          imgUrl: '/assets/images/shared/card/mandate.svg',
-          imgAlt: 'guarantor',
-          title: 'Client Guarantors',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: '/crm/clients/view-client-guarantor',
-        },
-        {
-          imgUrl: '/assets/images/shared/card/mandate.svg',
-          imgAlt: 'client status',
-          title: 'Client Statuses',
-          content:
-            'Introduce your company core info quickly to users by fill up company details',
-          link: '/crm/clients/view-client-statuses',
-        },
-      ],
+    const clientId = this.route.snapshot.paramMap.get('clientId');
+    this.originalCards = [
+      {
+        imgUrl: '/assets/images/shared/card/upload.svg',
+        imgAlt: 'upload',
+        title: 'Upload Documents',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: `/crm/clients/view-upload-documents/${clientId}`,
+      },
+      {
+        imgUrl: '/assets/images/shared/card/add.svg',
+        imgAlt: 'add',
+        title: 'Add Address',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: `crm/clients/view-client-addresses/${clientId}`,
+      },
+      {
+        imgUrl: '/assets/images/shared/card/sales.svg',
+        imgAlt: 'sales',
+        title: 'Sales Turnover',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: `/crm/clients/view-sales-turnover/${clientId}`,
+      },
+      {
+        imgUrl: '/assets/images/shared/card/phone.svg',
+        imgAlt: 'phone',
+        title: 'Phone Number',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: `/crm/clients/view-phone-number/${clientId}`,
+      },
+      {
+        imgUrl: '/assets/images/shared/card/contact.svg',
+        imgAlt: 'contact',
+        title: 'Contact Person',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: `/crm/clients/view-contact-person/${clientId}`,
+      },
+      {
+        imgUrl: '/assets/images/shared/card/cr.svg',
+        imgAlt: 'cr',
+        title: 'CR Authority Office',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: `/crm/clients/view-client-cr-authority-offices/${clientId}`,
+      },
+      {
+        imgUrl: '/assets/images/shared/card/tax.svg',
+        imgAlt: 'tax',
+        title: 'TAX Authority Office',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: `/crm/clients/view-client-tax-authority-offices/${clientId}`,
+      },
+      {
+        imgUrl: '/assets/images/shared/card/central.svg',
+        imgAlt: 'central',
+        title: 'Central Bank Info',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: `/crm/clients/view-client-central-bank-info/${clientId}`,
+      },
+      {
+        imgUrl: '/assets/images/shared/card/share.svg',
+        imgAlt: 'share',
+        title: 'Share Holders',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: `/crm/clients/view-client-share-holders/${clientId}`,
+      },
+      {
+        imgUrl: '/assets/images/shared/card/tml.svg',
+        imgAlt: 'tml',
+        title: 'TML Officer',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: `/crm/clients/view-client-tml-officers/${clientId}`,
+      },
+      {
+        imgUrl: '/assets/images/shared/card/mandate.svg',
+        imgAlt: 'guarantor',
+        title: 'Client Guarantors',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: '/crm/clients/view-client-guarantor',
+      },
+      {
+        imgUrl: '/assets/images/shared/card/mandate.svg',
+        imgAlt: 'client status',
+        title: 'Client Statuses',
+        content:
+          'Introduce your company core info quickly to users by fill up company details',
+        link: '/crm/clients/view-client-statuses',
+      },
     ];
+    this.cards = this.chunkArray(this.originalCards, 3);
+  }
+  onSearchClient(keyword: string) {
+    const lower = keyword.toLowerCase();
+    const filtered = this.originalCards.filter((card) =>
+      Object.values(card).some((val) =>
+        val?.toString().toLowerCase().includes(lower)
+      )
+    );
+
+    this.cards = this.chunkArray(filtered, 3); // 3 per row
+    console.log('Original Cards:', this.originalCards);
+    console.log('Chunked Cards:', this.cards);
+  }
+  chunkArray(arr: any[], chunkSize: number): any[][] {
+    const result = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      result.push(arr.slice(i, i + chunkSize));
+    }
+    return result;
   }
   navigateTo(link: string) {
     this.router.navigate([link]);
-  }
-  navigateToInfo() {
-    this.router.navigate(['/crm/clients/company-view-only']);
   }
 }
