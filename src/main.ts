cea40e904cc9332fb5b3ce78519a8cd3c07cb97d
walaa-zahran40 <walaa.zahran@corplease.com.eg>
2025-07-16 15:25:04 +0300

    production mode

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 /// <reference types="@angular/localize" />
+import { enableProdMode } from '@angular/core';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import { PublicClientApplication } from "@azure/msal-browser";
+import { PublicClientApplication } from '@azure/msal-browser';
+import { environment } from './environments/environment';
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule, {
@@ -12,10 +14,13 @@ platformBrowserDynamic()
 
 const msalInstance = new PublicClientApplication({
   auth: {
-    clientId: "your-client-id",
-    authority: "https://login.microsoftonline.com/your-tenant-id",
-    redirectUri: "http://localhost:3000"
-  }
+    clientId: 'your-client-id',
+    authority: 'https://login.microsoftonline.com/your-tenant-id',
+    redirectUri: 'http://localhost:3000',
+  },
 });
 
 msalInstance.initialize(); // 👈 This is REQUIRED before any other call
+if (environment.production) {
+  enableProdMode();
+}
