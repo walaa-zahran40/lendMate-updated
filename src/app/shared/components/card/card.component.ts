f94b11a6bad498fdad4019a9f09f86b140c2f513
walaa-zahran40 <walaa.zahran@corplease.com.eg>
2025-07-28 14:01:38 +0300

    card , delete module translation

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -3,10 +3,12 @@ import {
   EventEmitter,
   Input,
   OnChanges,
+  OnDestroy,
   Output,
   SimpleChanges,
 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-card',
@@ -14,7 +16,7 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent implements OnChanges {
+export class CardComponent implements OnChanges, OnDestroy {
   @Input() imgUrl!: string;
   @Input() imgAlt!: string;
   @Input() title!: string;
@@ -22,18 +24,25 @@ export class CardComponent implements OnChanges {
   @Output() navigation = new EventEmitter<void>();
   translatedTitle: string = '';
   translatedContent: string = '';
-  constructor(private translate: TranslateService) {}
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes['title']) {
-      this.translatedTitle = this.title
-        ? this.translate.instant(this.title)
-        : '';
-    }
+  private langSub!: Subscription;
 
-    if (changes['content']) {
-      this.translatedContent = this.content
-        ? this.translate.instant(this.content)
-        : '';
+  constructor(private translate: TranslateService) {
+    this.langSub = this.translate.onLangChange.subscribe(() => {
+      this.translateInputs();
+    });
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['title'] || changes['content']) {
+      this.translateInputs();
     }
+  }
+  private translateInputs() {
+    this.translatedTitle = this.title ? this.translate.instant(this.title) : '';
+    this.translatedContent = this.content
+      ? this.translate.instant(this.content)
+      : '';
+  }
+  ngOnDestroy(): void {
+    this.langSub?.unsubscribe();
   }
 }
