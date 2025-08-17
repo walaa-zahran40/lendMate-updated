e5faa639ccde1ab9c0679e0c04f7232ab755fa73
walaa-zahran40 <walaa.zahran@corplease.com.eg>
2025-08-17 11:17:32 +0300

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   // apiUrl: 'http://192.168.10.67:49170/api/',
   // apiUrl: 'https://192.168.10.67:446/api/',
-  apiUrl: 'https://192.168.10.67:7070/api/',
+  apiUrl: 'https://LendMate.Corplease.Com.eg:7070/api/',
   // apiUrl: 'http://192.168.10.67:49160/api/',
-  // redirectUri: 'http://localhost:4200',
-  redirectUri: 'LendMate.Corplease.Com.eg:444',
+  redirectUri: 'http://localhost:4200',
+  // redirectUri: 'https://LendMate.Corplease.Com.eg:444',
 };
